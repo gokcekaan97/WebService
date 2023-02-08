fbc4285efb86d371d5ef71fd934e97e2980c8ca9
Kaan GÖKÇEK <kaan.gokcek@halkbank.com.tr>
2023-02-08 11:14:32 +0300

    Initial Commit

---
+++ WebService/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebService
+//
+//  Created by kaan gokcek on 8.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
